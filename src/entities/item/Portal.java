cdc6703f9aaa5c872e8dc25f2ef9eda92a2ee730
Anh Duc <anhducpn67@gmail.com>
2020-12-15 09:21:52 +0700

    Final

--- src/entities/item/Portal.java
+++ src/entities/item/Portal.java
@@ -7,6 +7,7 @@ import entities.character.Bomber;
 import sprite.Sprite;
 
 public class Portal extends Item {
+
     public Portal(int x, int y, Sprite sprite) {
         super(x, y, sprite);
     }
